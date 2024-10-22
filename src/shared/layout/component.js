3c90275623992fcab99b232d2308f08c65b8894e
Thomas Greco <tg18509@pm.me>
2024-10-22 15:09:29 +0400

    fix(analytics): replaced implementation with correct one

--- src/shared/layout/component.js
+++ src/shared/layout/component.js
@@ -1,13 +1,11 @@
 import Navbar from "../../features/navbar/component";
 // import Footer from "../footer/component";
-import { Analytics } from "@vercel/analytics/react";
 
 const Layout = ({ children }) => (
   <>
     <div className="wrapper">
       <Navbar />
       <main className="main-content">{children}</main>
-      <Analytics />
     </div>
   </>
 );
