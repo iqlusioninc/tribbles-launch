e0037871201bb4475668a77743eedc2a7a42aae0
Thomas Greco <tg18509@pm.me>
2024-10-22 14:45:26 +0400

    feat(analytics): added vercel analyics component

--- src/shared/layout/component.js
+++ src/shared/layout/component.js
@@ -1,11 +1,13 @@
 import Navbar from "../../features/navbar/component";
-import Footer from "../footer/component";
+// import Footer from "../footer/component";
+import { Analytics } from "@vercel/analytics/react";
 
 const Layout = ({ children }) => (
   <>
     <div className="wrapper">
       <Navbar />
       <main className="main-content">{children}</main>
+      <Analytics />
     </div>
   </>
 );
